b21f8808cdc986e2e116437c0780898617f47ddd
Pedro Endo <endopedro@gmail.com>
2021-07-04 14:43:39 -0300

    chore: add mongo unified topology

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,8 @@ import { MongoClient } from 'mongodb'
 
 const connectToDatabase = async () => {
   const client = await MongoClient.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true }
   )
   return client
 }
