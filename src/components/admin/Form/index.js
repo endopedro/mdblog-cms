34ab35bfeca4c24c169e92b40b69f273e6c1444b
Pedro Endo <endopedro@gmail.com>
2021-07-17 16:02:26 -0300

    feat: export form components

---
+++ src/components/admin/Form/index.js
@@ -0,0 +1,6 @@
+import InputText from './InputText'
+import InputSelect from './InputSelect'
+import InputTags from './InputTags'
+import InputMarkdown from './InputMarkdown'
+
+export { InputText, InputSelect, InputTags, InputMarkdown }
