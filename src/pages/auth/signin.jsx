02241119c20ae58beb18697a58e504f517384d78
Pedro Endo <endopedro@gmail.com>
2021-07-17 21:53:27 -0300

    improvement: signin form

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -5,13 +5,11 @@ import {
   Title,
   Paper,
   Center,
-  TextInput,
-  PasswordInput,
   LoadingOverlay,
   Button,
   Text,
 } from '@mantine/core'
-import { useForm } from 'react-hook-form'
+import { useForm, FormProvider } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { signIn } from 'next-auth/client'
@@ -25,6 +23,7 @@ import {
 import { useNotifications } from '@mantine/notifications'
 
 import Layout from '../../components/auth/Layout'
+import { InputText, InputPassword } from '../../components/admin/Form'
 
 const schema = yup.object().shape({
   email: yup.string().required('Type an e-mail.').email('Type a valid e-mail.'),
@@ -37,13 +36,17 @@ const schema = yup.object().shape({
 const Login = () => {
   const router = useRouter()
   const notifications = useNotifications()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) })
-
+  const methods = useForm({ resolver: yupResolver(schema) })
   const [loading, setLoading] = useState(false)
+
+  const notify = (success = true, message) => {
+    notifications.showNotification({
+      title: success ? 'Logged in' : 'Login Fail',
+      message: message,
+      color: success ? 'blue' : 'red',
+      icon: success ? <RiCheckLine /> : <RiCloseLine />,
+    })
+  }
 
   const onSubmit = async (data) => {
     setLoading(true)
@@ -53,19 +56,8 @@ const Login = () => {
       password: data.password,
     })
     setLoading(false)
-    if (!result.error) {
-      notifications.showNotification({
-        title: 'Logged in',
-        icon: <RiCheckLine />,
-      })
-      router.replace('/admin')
-    } else
-      notifications.showNotification({
-        title: 'Login Fail',
-        color: 'red',
-        message: result.error,
-        icon: <RiCloseLine />,
-      })
+    notify(!result.erro)
+    if (!result.error) router.replace('/admin')
   }
 
   return (
@@ -79,50 +71,47 @@ const Login = () => {
         </Title>
         <Paper padding="lg" shadow="md" className="w-5/6 sm:w-96 mb-16">
           <LoadingOverlay visible={loading} />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-5">
-              <TextInput
-                {...register('email')}
-                placeholder="e-mail"
-                label="E-mail "
-                radius="xl"
-                required
-                className="mb-7"
-                disabled={loading}
-                icon={<RiMailLine />}
-                error={errors.email?.message}
-              />
-              <PasswordInput
-                {...register('password')}
-                placeholder="password"
-                label="Password"
-                radius="xl"
-                required
-                type="submit"
-                disabled={loading}
-                icon={<RiLockPasswordLine />}
-                error={errors.password?.message}
-              />
-            </div>
-            <div></div>
-            <div className="flex justify-between items-center">
-              <Link href="/auth/forgot-password">
-                <a>
-                  <Text size="xs">Forgot password?</Text>
-                </a>
-              </Link>
-              <Button
-                variant="light"
-                color="indigo"
-                radius="lg"
-                disabled={loading}
-                type="submit"
-                className="ml-auto"
-              >
-                Enter
-              </Button>
-            </div>
-          </form>
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)}>
+              <div className="mb-5">
+                <InputText
+                  name='email'
+                  placeholder="e-mail"
+                  label="E-mail"
+                  required
+                  className="mb-3"
+                  disabled={loading}
+                  icon={<RiMailLine />}
+                />
+                <InputPassword
+                  className='mb-7'
+                  name='password'
+                  placeholder="password"
+                  required
+                  type="submit"
+                  disabled={loading}
+                  icon={<RiLockPasswordLine />}
+                />
+              </div>
+              <div className="flex justify-between items-center">
+                <Link href="/auth/forgot-password">
+                  <a>
+                    <Text size="xs">Forgot password?</Text>
+                  </a>
+                </Link>
+                <Button
+                  variant="light"
+                  color="indigo"
+                  radius="lg"
+                  disabled={loading}
+                  type="submit"
+                  className="ml-auto"
+                >
+                  Enter
+                </Button>
+              </div>
+            </form>
+          </FormProvider>
         </Paper>
       </Center>
     </Layout>
