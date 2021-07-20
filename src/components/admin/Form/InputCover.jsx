54c8077223ac99f75335011c3a29f83ad2af7fe8
Pedro Endo <endopedro@gmail.com>
2021-07-20 19:06:11 -0300

    feat: input cover

--- src/components/admin/Form/InputCover.jsx
+++ src/components/admin/Form/InputCover.jsx
@@ -1,26 +1,66 @@
-import React from 'react'
-import { Image, Button } from '@mantine/core'
+import React, { useState } from 'react'
+import { Image, Button, ActionIcon } from '@mantine/core'
 import { useFormContext } from 'react-hook-form'
+import cx from 'classnames'
+import { RiCloseFill, RiEdit2Fill } from 'react-icons/ri'
 
 import GalleryModal from '../GalleryModal'
 
 const InputCover = ({ className, name }) => {
   const methods = useFormContext()
+  const [opened, setOpened] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const onSelect = (image) => {
+    methods.setValue(name, image ? image._id : null)
+    setSelectedImage(image)
+    setOpened(false)
+  }
 
   return (
-    <div className={className}>
+    <div className={cx('relative', className)}>
       <input type="hidden" {...methods.register(name)} />
       <Image
         // width={200}
         height={120}
-        src={null}
+        src={selectedImage?.secure_url}
         alt="With default placeholder"
         withPlaceholder
         styles={{ placeholder: { backgroundColor: '#1D1E30' } }}
         radius="md"
-        placeholder={<Button variant="outline">Upload Cover</Button>}
+        placeholder={
+          <Button variant="outline" onClick={setOpened}>
+            Upload Cover
+          </Button>
+        }
       />
-      <GalleryModal />
+      {selectedImage && (
+        <div className="absolute right-0 bottom-0 flex">
+          <ActionIcon
+            variant="filled"
+            color="blue"
+            radius="xl"
+            onClick={() => setOpened(true)}
+          >
+            <RiEdit2Fill />
+          </ActionIcon>
+          <ActionIcon
+            className="ml-1"
+            variant="filled"
+            color="red"
+            radius="xl"
+            onClick={() => onSelect(null)}
+          >
+            <RiCloseFill />
+          </ActionIcon>
+        </div>
+      )}
+      <GalleryModal
+        opened={opened}
+        setOpened={setOpened}
+        onSelect={onSelect}
+        selectedImage={selectedImage}
+      />
     </div>
   )
 }
