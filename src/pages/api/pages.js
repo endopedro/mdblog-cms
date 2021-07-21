5169d4b9b92bddf53d432e66db3c9e0d3ac791b3
Pedro Endo <endopedro@gmail.com>
2021-07-21 00:56:34 -0300

    improvement: service to add cover to page

--- src/pages/api/pages.js
+++ src/pages/api/pages.js
@@ -45,7 +45,7 @@ const handler = async (req, res) => {
     }
 
     const data = req.body
-    const { title, slug, content } = data
+    const { title, slug, content, cover_image } = data
 
     if (!title || !slug) {
       res.status(422).json({ message: 'Incomplete information.' })
@@ -68,6 +68,7 @@ const handler = async (req, res) => {
       slug: slug,
       content: content,
       createdAt: Date.now(),
+      cover_image: cover_image,
     })
 
     res.status(200).json({ message: 'Page created!', page: result.ops[0] })
@@ -82,7 +83,7 @@ const handler = async (req, res) => {
     }
 
     const data = req.body
-    const { _id, title, slug, content } = data
+    const { _id, title, slug, content, cover_image } = data
 
     if (!title || !slug) {
       res.status(422).json({ message: 'Incomplete information.' })
@@ -110,6 +111,7 @@ const handler = async (req, res) => {
           slug: slug,
           content: content,
           updatedAt: Date.now(),
+          cover_image: cover_image,
         },
       },
       {
