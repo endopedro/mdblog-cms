719133d1f1e04ca24ee89e2b1fa5a9c6da833e12
Pedro Endo <endopedro@gmail.com>
2021-07-26 20:32:46 -0300

    improvement: settings page

--- src/pages/admin/settings.jsx
+++ src/pages/admin/settings.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Layout from '../../components/admin/Layout'
+import Form from '../../domain/settings/Form'
+import { EditResource } from '../../components/admin/Resource'
+import { data } from '../../states/session'
 
-const Settings = () => {
-  return <Layout page="Settings">Settings</Layout>
+import settingsApi from '../../services/settingsApi'
+
+const Profile = () => {
+  const session = data.use()
+  const [settings, setSettings] = useState(null)
+
+  useEffect(async () => {
+    if (session) {
+      settingsApi()
+        .getSettings()
+        .then(({ data }) => setSettings(data.result))
+        .catch(() => null)
+    }
+  }, [session])
+
+  return (
+    <Layout page="Settings">
+      <EditResource
+        Form={Form}
+        name="settings"
+        content={settings}
+        title="Site Settings"
+      />
+    </Layout>
+  )
 }
 
-export default Settings
+export default Profile
