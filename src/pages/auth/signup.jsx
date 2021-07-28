097c5774e05e7e388c929d3668424f579b56836e
Pedro Endo <endopedro@gmail.com>
2021-07-27 22:07:36 -0300

    feat: control register page

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
-import { Title, Paper, Center, Button, LoadingOverlay } from '@mantine/core'
+import {
+  Title,
+  Paper,
+  Center,
+  Button,
+  LoadingOverlay,
+  Loader,
+} from '@mantine/core'
 import { useForm, FormProvider } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
@@ -18,6 +25,7 @@ import { useRouter } from 'next/router'
 
 import Layout from '../../components/auth/Layout'
 import { InputText, InputPassword } from '../../components/admin/Form'
+import settingApi from '../../services/settingsApi'
 
 const schema = yup.object().shape({
   name: yup
@@ -40,6 +48,7 @@ const SignUp = () => {
   const notifications = useNotifications()
   const methods = useForm({ resolver: yupResolver(schema) })
   const [loading, setLoading] = useState(false)
+  const [permit, setPermit] = useState(false)
 
   const notify = (success = true, message) => {
     notifications.showNotification({
@@ -54,13 +63,26 @@ const SignUp = () => {
     setLoading(true)
     await userApi()
       .createUser(data)
-      .then(response => {
+      .then((response) => {
         notify(true, response.data.message)
         router.replace('/auth/signin')
       })
-      .catch(error => notify(false, error.response.data.message))
+      .catch((error) => notify(false, error.response.data.message))
       .finally(() => setLoading(false))
   }
+
+  useEffect(async () => {
+    settingApi()
+      .getMetrics()
+      .then(({ data }) => {
+        if (data.result.users > 0) router.replace('/auth/signin')
+        else setPermit(true)
+      })
+      .catch(() => {
+        notify(false, 'Connection failure')
+        router.replace('/auth/signin')
+      })
+  }, [])
 
   return (
     <Layout>
@@ -72,61 +94,67 @@ const SignUp = () => {
           Register
         </Title>
         <Paper padding="lg" shadow="md" className="w-5/6 sm:w-96 mb-8">
-          <LoadingOverlay visible={loading} />
-          <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)}>
-              <div className="mb-5">
-                <InputText
-                  name="name"
-                  placeholder="name"
-                  required
-                  maxLength="50"
-                  className="mb-3"
-                  icon={<RiUserLine />}
-                  disabled={loading}
-                />
-                <InputText
-                  name="email"
-                  placeholder="e-mail"
-                  label="E-mail"
-                  required
-                  maxLength="50"
-                  className="mb-3"
-                  disabled={loading}
-                  icon={<RiMailLine />}
-                />
-                <InputPassword
-                  name="password"
-                  placeholder="password"
-                  required
-                  maxLength="50"
-                  className="mb-3"
-                  disabled={loading}
-                  icon={<RiLockPasswordLine />}
-                />
-                <InputPassword
-                  name="confirm_password"
-                  placeholder="confirm password"
-                  label="Confirm Password"
-                  required
-                  maxLength="50"
-                  disabled={loading}
-                  icon={<RiLockPasswordFill />}
-                  type="submit"
-                />
-              </div>
-                <Button
-                  variant="light"
-                  color="indigo"
-                  radius="lg"
-                  type="submit"
-                  disabled={loading}
-                  className="float-right"
-                >
-                  Enter
-                </Button>
-            </form>
-          </FormProvider>
+          {permit ? (
+            <>
+              <LoadingOverlay visible={loading} />
+              <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit(onSubmit)}>
+                  <div className="mb-5">
+                    <InputText
+                      name="name"
+                      placeholder="name"
+                      required
+                      maxLength="50"
+                      className="mb-3"
+                      icon={<RiUserLine />}
+                      disabled={loading}
+                    />
+                    <InputText
+                      name="email"
+                      placeholder="e-mail"
+                      label="E-mail"
+                      required
+                      maxLength="50"
+                      className="mb-3"
+                      disabled={loading}
+                      icon={<RiMailLine />}
+                    />
+                    <InputPassword
+                      name="password"
+                      placeholder="password"
+                      required
+                      maxLength="50"
+                      className="mb-3"
+                      disabled={loading}
+                      icon={<RiLockPasswordLine />}
+                    />
+                    <InputPassword
+                      name="confirm_password"
+                      placeholder="confirm password"
+                      label="Confirm Password"
+                      required
+                      maxLength="50"
+                      disabled={loading}
+                      icon={<RiLockPasswordFill />}
+                      type="submit"
+                    />
+                  </div>
+                  <Button
+                    variant="light"
+                    color="indigo"
+                    radius="lg"
+                    type="submit"
+                    disabled={loading}
+                    className="float-right"
+                  >
+                    Enter
+                  </Button>
+                </form>
+              </FormProvider>
+            </>
+          ) : (
+            <Loader className="mx-auto" />
+          )}
         </Paper>
       </Center>
     </Layout>
