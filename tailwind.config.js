d811654b65d97c5792bb80cd218705b692db6047
Pedro Endo <endopedro@gmail.com>
2021-08-12 17:30:05 -0300

    chore: tailwind blog color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -7,6 +9,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      woodsmoke: {
+        50: '#383B3F',
+        100: '#35373B',
+        200: '#2E3033',
+        300: '#26282B',
+        400: '#1F2123',
+        500: '#18191B',
+        600: '#111213',
+        700: '#0A0A0B',
+        800: '#020303',
+        900: '#000000',
+      },
+    },
   },
   variants: {
     extend: {},
