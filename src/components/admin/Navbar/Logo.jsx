8740e2a217c189691b1bcdd310d21d8fc1f63402
Pedro Endo <endopedro@gmail.com>
2021-08-16 17:12:13 -0300

    fix: logo element

--- src/components/admin/Navbar/Logo.jsx
+++ src/components/admin/Navbar/Logo.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
 import { Text } from '@mantine/core'
 
-import { data } from '../../../states/settings'
-
-const Logo = () => {
-  const settings = data.use()
-
-  return (
-    <div className="flex-shrink-0 flex items-center" style={{ width: '80px' }}>
-      {settings?.logo ? (
-        <img
-          className="h-8 w-auto mx-auto md:mx-0"
-          src={settings ? settings.logo?.data.secure_url : ''}
-          alt="logo"
-        />
-      ) : (
-        <Text size="lg" weight={700} color="blue">
-          MDBLOG
-        </Text>
-      )}
-    </div>
-  )
-}
+const Logo = () => (
+  <div className="flex-shrink-0 flex items-center" style={{ width: '80px' }}>
+    <Text size="lg" weight={700} color="blue">
+      MDBLOG
+    </Text>
+  </div>
+)
 
 export default Logo
